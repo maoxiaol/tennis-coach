b1258f9a4e2470decd6e43c5a91d25af1147efff
TennisCoach <coach@tennis.app>
2026-06-09 22:41:40 +0800

    cleanup: remove temporary debug files

--- test_val.js
+++
@@ -1,5 +0,0 @@
-function test() {
-      return `<div class="card" style="cursor:pointer;padding:10px;margin-bottom:4px;" onclick="selectAiVideo('' + v.url.replace(/'/g, '') + '',' + v.id + ',\"' + (v.note||'').replace(/"/g, '') + '\")">`  +
-  console.log('OK');
-}
-test();
